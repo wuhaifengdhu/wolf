974210d94a38e2ffcc2ca5d53fb0a4c0d39f1d48
haifwu <haifwu@ebay.com>
2014-10-28 17:54:02 +0800

    basic edition based on weixin-robot

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
-var app = require("express").createServer();
-var wx = require('./lib/wx');
-var fs = require("fs");
-app.get('/', function(req, response) {
+var express = require('express');
+var webot = require('weixin-robot');
 
-    fs.readFile("./qrCode.jpg", "binary", function(err, file) {
-        if (err) {
-            response.writeHead(500, {
-                "Content-Type": "text/plain"
-            });
-            response.write(err + "\n");
-        } else {
-            response.writeHead(200, {
-                "Content-Type": "image/jpg"
-            });
-            response.write(file, "binary");
-        }
-        response.end();
-    });
+var app = express();
+
+// 指定回复消息
+webot.set('hi', '你好');
+
+webot.set('subscribe', {
+  pattern: function(info) {
+    return info.is('event') && info.param.event === 'subscribe';
+  },
+  handler: function(info) {
+    return '欢迎订阅微信机器人';
+  }
 });
 
+webot.set('test', {
+  pattern: /^test/i,
+  handler: function(info, next) {
+    next(null, 'roger that!')
+  }
+})
 
-app.get('/weixin', function(req, res) {
-    var signature = req.query.signature;
-    var timestamp = req.query.timestamp;
-    var nonce = req.query.nonce;
-    var echostr = req.query.echostr;
-    var check = false;
-    check = wx.isLegel(signature, timestamp, nonce, 'wolf'); //替换成你的token
-    if (check) {
-        res.write(echostr);
-    } else {
-        res.write("error data");
-    }
-    res.end();
+// 你可以获取已定义的 rule
+//
+// webot.get('subscribe') ->
+//
+// {
+//   name: 'subscribe',
+//   pattern: function(info) {
+//     return info.is('event') && info.param.event === 'subscribe';
+//   },
+//   handler: function(info) {
+//     return '欢迎订阅微信机器人';
+//   }
+// }
+//
+
+// 接管消息请求
+webot.watch(app, { token: 'wolf', path: '/wechat' });
+
+// 如果需要多个实例（即为多个微信账号提供不同回复）：
+var webot2 = new webot.Webot();
+webot2.set({
+  '/hi/i': 'Hello',
+  '/who (are|r) (you|u)/i': 'I\'m a robot.'
+});
+webot2.watch(app, {
+  token: 'token2',
+  path: '/wechat_en', // 这个path不能为之前已经监听过的path的子目录
 });
 
+// 启动 Web 服务
+// 微信后台只允许 80 端口
+app.listen(80);
 
-app.post('/weixin', function(req, res) {
-    var response = res;
-    var formData = "";
-    req.on("data", function(data) {
-        formData += data;
-    });
-    req.on("end", function() {
-        wx.processMessage(formData, response);
-    });
-});
-app.listen(3000);
+// 如果你不想让 node 应用直接监听 80 端口
+// 可以尝试用 nginx 或 apache 自己做一层 proxy
+// app.listen(process.env.PORT);
+// app.enable('trust proxy');
