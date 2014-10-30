98abb31020ffe293d1ef759e10ad938037da2bc9
haifwu <haifwu@ebay.com>
2014-10-30 10:13:26 +0800

    clean some unused things

--- index.js
+++ index.js
@@ -22,40 +22,11 @@ webot.set('test', {
   }
 })
 
-// 你可以获取已定义的 rule
-//
-// webot.get('subscribe') ->
-//
-// {
-//   name: 'subscribe',
-//   pattern: function(info) {
-//     return info.is('event') && info.param.event === 'subscribe';
-//   },
-//   handler: function(info) {
-//     return '欢迎订阅微信机器人';
-//   }
-// }
-//
 
 // 接管消息请求
 webot.watch(app, { token: 'wolf', path: '/wechat' });
-
-// 如果需要多个实例（即为多个微信账号提供不同回复）：
-var webot2 = new webot.Webot();
-webot2.set({
-  '/hi/i': 'Hello',
-  '/who (are|r) (you|u)/i': 'I\'m a robot.'
-});
-webot2.watch(app, {
-  token: 'token2',
-  path: '/wechat_en', // 这个path不能为之前已经监听过的path的子目录
-});
 
 // 启动 Web 服务
 // 微信后台只允许 80 端口
 app.listen(80);
 
-// 如果你不想让 node 应用直接监听 80 端口
-// 可以尝试用 nginx 或 apache 自己做一层 proxy
-// app.listen(process.env.PORT);
-// app.enable('trust proxy');
